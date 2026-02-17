1a8bd7473f2e5c0e23ab89f9658279a081eca20f
MAYANK PANDEY <101644584+mayank2021264@users.noreply.github.com>
2026-02-17 13:19:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function leapyear(year) {
+	if(year%400 === 0) return true;
+	else if(year%100===0) return false;
+	else if(year%4===0) return true;
+	return false;
+}
+function daysOfAYear(year) {
+	if(leapyear(year)) return 366;
+	return 365;
+}
